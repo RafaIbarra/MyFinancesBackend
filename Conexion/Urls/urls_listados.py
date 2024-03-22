a8d07d7920e2c77d914d94b1f443d1577bf050e0
RafaIbarra <blasrafael1986@gmail.com>
2024-03-21 23:09:52 -0400

    decima prueba seleccion carpeta

--- Conexion/Urls/urls_listados.py
+++ Conexion/Urls/urls_listados.py
@@ -7,13 +7,14 @@ from django.urls import path
 # from ..Apis.Listados.api_listados import resumen,balance,meses,misgastos,misegresos
 
 
-from Conexion.Apis.Listados.api_listados import misproductosfinancieros,misingresos,misdatosregistroegreso,miscategorias,resumen,balance,meses,misgastos,misegresos
+# from Conexion.Apis.Listados.api_listados import misproductosfinancieros,misingresos,misdatosregistroegreso,miscategorias,resumen,balance,meses,misgastos,misegresos
+from Conexion.Apis.Listados.api_listados import misproductosfinancieros,misingresos,misdatosregistroegreso,miscategorias
 from Conexion.Apis.Estadisticas.api_estadisticas import *
 urlpatterns = [
 
-    path('Meses/',meses,name='meses'),
-    path('MisGastos/',misgastos,name='misgastos'),
-    path('MisEgresos/<int:anno>/<int:mes>/',misegresos,name='misegresos'),
+    # path('Meses/',meses,name='meses'),
+    # path('MisGastos/',misgastos,name='misgastos'),
+    # path('MisEgresos/<int:anno>/<int:mes>/',misegresos,name='misegresos'),
 
     path('MisDatosRegistroEgreso/',misdatosregistroegreso,name='misdatosregistroegreso'),
     path('MisCategorias/',miscategorias,name='miscategorias'),
@@ -24,7 +25,7 @@ urlpatterns = [
     path('MisProductosFinancieros/',misproductosfinancieros,name='misproductosfinancieros'),
     path('MisIngresos/<int:anno>/<int:mes>/',misingresos,name='misingresos'),
 
-    path('Balance/<int:anno>/<int:mes>/',balance,name='balance'),
-    path('Resumen/<int:anno>/<int:mes>/',resumen,name='resumen'),
+    # path('Balance/<int:anno>/<int:mes>/',balance,name='balance'),
+    # path('Resumen/<int:anno>/<int:mes>/',resumen,name='resumen'),
 
 ]
