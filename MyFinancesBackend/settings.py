16a747001e9a28c67128783d7a1f85be0c5289ea
Rafael Ibarra <rafaeli@coopcapiata.local>
2024-03-22 11:26:12 -0300

    permiso para el fronted en el cors

--- MyFinancesBackend/settings.py
+++ MyFinancesBackend/settings.py
@@ -198,6 +198,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     'http://localhost:3000',
     'http://localhost:5173',
+    'https://my-finances-web-btxv.vercel.app/',
     
     
     
