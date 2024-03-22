38892b23982d5b670595a79a0ff41b66195e1e02
RafaIbarra <blasrafael1986@gmail.com>
2024-03-21 23:12:28 -0400

    decimo primera prueba seleccion carpeta

--- MyFinancesBackend/urls.py
+++ MyFinancesBackend/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/login/',Login.as_view(),name="Login"),
     path('api/Registro/',Registro.as_view(),name="Registro"), 
-    path('api/',include("Conexion.Urls.urls_listados")), 
+    # path('api/',include("Conexion.Urls.urls_listados")), 
     path('api/',include("Conexion.Urls.urls_operaciones")), 
     
     
