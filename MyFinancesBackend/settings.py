7a70097993a04cdb268e59a345a17f493b3c9509
Rafael Ibarra <rafaeli@coopcapiata.local>
2024-03-22 10:24:04 -0300

    permisos al localhost

--- MyFinancesBackend/settings.py
+++ MyFinancesBackend/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+    ALLOWED_HOSTS.append('http://localhost:5173/')
 
 # Application definition
 
